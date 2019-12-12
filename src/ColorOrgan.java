6eb2e0221779659fd8c4c0c81cb1c6e0b593b3fc
Samuel <samuel.oosterholt20001004@gmail.com>
2019-12-12 12:46:33 +0100

    Moved over from Processing IDE to IntellIJ.

---
+++ src/ColorOrgan.java
@@ -0,0 +1,25 @@
+import processing.core.PApplet;
+
+public class ColorOrgan extends PApplet {
+
+    // The argument passed to main must match the class name
+    public static void main(String[] args) {
+        PApplet.main("ColorOrgan");
+    }
+
+    // method used only for setting the size of the window
+    public void settings(){
+
+    }
+
+    // identical use to setup in Processing IDE except for size()
+    public void setup(){
+
+    }
+
+    // identical use to draw in Prcessing IDE
+    public void draw(){
+
+    }
+
+}
